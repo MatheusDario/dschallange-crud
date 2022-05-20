fa1656c9c5ed5e37b196de6adb9fcf6d5200599b
MatheusDario <dario.matheus97@gmail.com>
2022-05-20 19:19:03 -0300

    First commit

---
+++ backend/src/main/java/com/devsuperior/dschallenge/DschallengeApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DschallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DschallengeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dschallenge/DschallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dschallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DschallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
