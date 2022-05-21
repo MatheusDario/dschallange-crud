8c836635e3227885beafa74faf447412f207d27b
MatheusDario <dario.matheus97@gmail.com>
2022-05-21 12:43:51 -0300

    insert

--- backend/src/main/java/com/devsuperior/dschallenge/resouces/ClientResouce.java
+++ backend/src/main/java/com/devsuperior/dschallenge/resouces/ClientResouce.java
@@ -1,12 +1,15 @@
 package com.devsuperior.dschallenge.resouces;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +17,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dschallenge.dto.ClientDTO;
 import com.devsuperior.dschallenge.services.ClientService;
-
-import java.net.URI;
 
 @RestController
 @RequestMapping(value ="/clients")
@@ -42,5 +43,17 @@ public class ClientResouce {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping(value ="/{id}")
+	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- backend/src/main/java/com/devsuperior/dschallenge/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dschallenge/services/ClientService.java
@@ -5,30 +5,32 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dschallenge.dto.ClientDTO;
 import com.devsuperior.dschallenge.entities.Client;
 import com.devsuperior.dschallenge.repositories.ClientRepository;
-import com.devsuperior.dschallenge.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dschallenge.services.exceptions.ResouceNotFoundException;
 
 @Service
 public class ClientService {
-	
+
 	@Autowired
 	private ClientRepository repository;
-	
+
 	@Transactional(readOnly = true)
 	public List<ClientDTO> findAll() {
-		List<Client> list = repository.findAll();	
+		List<Client> list = repository.findAll();
 		return list.stream().map(cls -> new ClientDTO(cls)).collect(Collectors.toList());
 	}
 
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
-		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Client entity = obj.orElseThrow(() -> new ResouceNotFoundException("Entity not found"));
 		return new ClientDTO(entity);
 	}
 
@@ -38,7 +40,29 @@ public class ClientService {
 		copyToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
-	
+
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getById(id);
+			copyToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		} catch (javax.persistence.EntityNotFoundException e) {
+			throw new ResouceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+            throw new ResouceNotFoundException("Id not found " + id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataBaseException("Integrity violation");
+        }
 	}
 
 	private void copyToEntity(ClientDTO dto, Client entity) {
@@ -48,4 +72,5 @@ public class ClientService {
 		entity.setIncome(dto.getIncome());
 		entity.setChildren(dto.getChildren());
 	}
+
 }
